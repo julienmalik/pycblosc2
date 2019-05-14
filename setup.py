2fe86593f1bea24c9f1ca3b9fb44443db997b3b9
Julien Malik <julien.malik@unseenlabs.fr>
2019-05-14 18:06:00 +0200

    do not import setuptools_scm in setup.py, according to documentation

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
-import setuptools_scm
-from setuptools_scm import get_version
 from distutils.core import setup
-
 
 setup(
     name='pycblosc2',
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
-    version=get_version(),
     py_modules=['pycblosc2'],
     )
