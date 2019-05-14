7a84575dabf9ebbdb19390c22f6d435eb66c71e7
Julien Malik <julien.malik@unseenlabs.fr>
2019-05-14 18:32:27 +0200

    add cffi to install_requires

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='pycblosc2',
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
-    install_requires=['setuptools_scm'],
+    install_requires=['setuptools_scm', 'cffi'],
     py_modules=['pycblosc2'],
     )
